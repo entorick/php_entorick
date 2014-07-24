d2898d5d310ed6f95a6125c01c715437961307aa
entorick <entorick11@qq.com>
2014-07-24 21:18:27 +0800

    htmlspecialchars反解加入深度参数

--- lib/Util/Decode.php
+++ lib/Util/Decode.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * decode相关的通用函数
+ * 各种decode相关的通用函数
  * @author entorick11@qq.com
  */
 
@@ -8,10 +8,17 @@ class Util_Decode{
     /*
      * 字符串在各层之间被多次htmlspecialchars
      * 用此方法decode
+     * @params string $str    // 要转化的字符串
+     * @parasm int    $level  // 反解的层级，默认false，表示无限
      */
-    public static function multiHtmlSpecialCharsDecode($str){
+    public static function multiHtmlSpecialCharsDecode($str, $level = false){
         while($str != htmlspecialchars_decode($str)){
             $str = htmlspecialchars_decode($str);
+            if ($level !== false && $level > 0){
+                $level--;
+            } elseif ($level !== false){
+                return $str;
+            }
         }
         return $str;
     }
