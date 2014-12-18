5d4322e4dba7dcbaa9900d3a3d00b449ca895e31
entorick11 <entorick11@qq.com>
2014-12-18 19:56:05 +0800

    add learning's for future review

---
+++ learning/objectIterator.php
@@ -0,0 +1,18 @@
+<?php
+
+class Sample implements Iterator{
+    private $_items = array(1,2,3,4,5,6,7);
+     
+    public function __construct() {}
+
+    public function rewind() { reset($this->_items); }
+    public function current() { return current($this->_items); }
+    public function key() { return key($this->_items); }
+    public function next() { return next($this->_items); }
+    public function valid() { return ( $this->current() !== false ); }
+}
+
+$sa = new Sample();
+foreach ($sa as $key => $val){
+    print $key . "=>" . $val . "\n";
+}
